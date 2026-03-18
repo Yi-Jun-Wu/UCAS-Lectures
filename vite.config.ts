15589db56dece86d7f33edc36d5eb027890607be
Yi-Jun-Wu <wuyijun21@mails.ucas.edu.cn>
2026-03-19 00:09:02 +0800

    deploy!!

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Use a relative path for all assets to ensure correct loading in Electron
   plugins: [
     react(),
     tailwindcss(),

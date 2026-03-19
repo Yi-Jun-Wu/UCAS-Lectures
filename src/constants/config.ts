8025911e7290b87f5ab2bc55c03d6995d38b8532
Yi-Jun-Wu <133572434+Yi-Jun-Wu@users.noreply.github.com>
2026-03-19 16:44:46 +0800

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -8,6 +8,8 @@ export const API_ENDPOINTS = {
 export const STORAGE_KEYS = {
   STARRED_LECTURES: 'ucas_starred_lectures_v_one',
   CACHED_LECTURES_DATA: 'ucas_lectures_raw_cache_data', // 存储完整数据的键
+  LECTURE_FILTERS: 'ucas_lectures_filters',
+  UI_SCALE: ''ucas_lectures_ui_scale',
 };
 
 // 日历视图的时间边界 (24小时制)
